5c5920589e1efe40f8c245803b9617dac339be6a
Yash Raj <yash524767@gmail.com>
2024-06-19 11:31:03 +0530

    Lecture 2

---
+++ Lecture2-CipherSchool/Main.java
@@ -0,0 +1,18 @@
+package coreJava;
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        System.out.println("X is:" + x);
+        System.out.println("Y is: "+ y);
+
+        byte b = 10;
+        System.out.println("B is: "+ b);
+        
+        byte b2 = 120;
+        System.out.println("B2 is :"+b2);
+
+        int i = 45;
+        System.out.println("I is: "+i);;
+    }
+}
