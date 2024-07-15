671fcfa6a18446330e3917764d26e9dc426b52eb
Yash Raj <yash524767@gmail.com>
2024-07-15 10:21:40 +0530

    Updated

--- Lecture11-CipherSchool/AbstractStudy.java
+++ Lecture11-CipherSchool/AbstractStudy.java
@@ -1,6 +1,7 @@
 abstract class Car{
     int maxSpeed;
     abstract void acclerate();
+    abstract void brake();
 }
 
 abstract class BMW extends Car{
@@ -13,6 +14,18 @@ class BMW2 extends BMW{
         System.out.println("BMW is applying brakes");
     }
 }
+abstract class A{//an abstract class can exits without abstract methods
+    //we simply mean we will not be able to create object of this class
+    int x = 10;
+    void fun(){
+        System.out.println("Hello, I am A");
+    }
+}
+class B extends A{
+    public void call(){
+        super.fun();
+    }
+}
 
 public class AbstractStudy {
     public static void main(String[] args) {
@@ -21,6 +34,8 @@ public class AbstractStudy {
         BMW2 b = new BMW2();
         b.acclerate();
         b.brake();
+        B b1 = new B();
+        b1.call();
 
     }
 }
