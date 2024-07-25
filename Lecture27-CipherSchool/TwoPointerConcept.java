d26fa864bf13657d316378920f92c0794241e666
Yash Raj <yash524767@gmail.com>
2024-07-25 23:17:58 +0530

    done

---
+++ Lecture27-CipherSchool/TwoPointerConcept.java
@@ -0,0 +1,23 @@
+public class TwoPointerConcept {
+  public static void main(String[] args) {
+    int arr[] = { 10, 20, 35, 50, 75, 80 };
+    int x = 95;
+    int l = 0;
+    int r = arr.length - 1;
+    boolean found = false;
+
+    while (l < r) {
+      int sum = arr[l] + arr[r];
+      if (sum == x) {
+        System.out.println("Found" + x + "as sum of values on index: " + l + "and Index: " + r);
+        return;
+      } else if (sum < x) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+    if (!found)
+      System.out.println("Sum not found.");
+  }
+}
