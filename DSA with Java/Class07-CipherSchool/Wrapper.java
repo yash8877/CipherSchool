532bf72712dfb89717ad67f2ea852709f8ca7585
Yash Raj <yash524767@gmail.com>
2024-08-04 14:53:50 +0530

    dome

---
+++ DSA with Java/Class07-CipherSchool/Wrapper.java
@@ -0,0 +1,9 @@
+public class Wrapper {
+
+    public static void main(String[] args) {
+        Integer x1 = new Integer(x1);
+        System.out.println(x1);
+        //creating object out if varibale x
+        //warpping the primitive data type 
+    }
+}
