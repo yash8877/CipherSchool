6e16910b9e4ba9d345bd734cdc526a2739a6bcd1
Yash Raj <yash524767@gmail.com>
2024-08-07 10:35:46 +0530

    done

---
+++ DSA with Java/Class13-CipherSchool/InterfaceStudy.java
@@ -0,0 +1,47 @@
+/**
+ * InnerInterfaceStudy
+ */
+interface M {
+    int x = 10;
+    void fun();
+    
+}
+interface N{
+    int x = 20;
+    void fun();
+    
+}
+
+class C implements M,N{//multiple inheritence inn java
+    public void fun(){
+        System.out.println("I am function in C");
+        System.out.println(M.x);//Ambiguity resolved using fully qualified name
+        System.out.println(N.x);//Ambiguity resolved using fully qualified name
+        System.out.println(M.super.hashCode());
+        System.out.println(N.super.hashCode());
+
+    }
+}
+/**
+ * InnerInterfaceStudy
+ */
+interface  O extends M,N{
+    void fun();
+    
+}
+class D implements O{
+    public void fun(){
+        System.out.println("I am in function D");
+    }
+}
+
+
+public class InterfaceStudy {
+
+    public static void main(String[] args) {
+        C c = new C();
+        c.fun();
+        D d = new D();
+        d.fun();
+    }
+}
